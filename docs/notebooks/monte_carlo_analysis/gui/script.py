167bb1b4b991e417cdf55521f1ba5ad5b1d7b89e
Jaival <pateljaival05@gmail.com>
2024-07-17 15:50:44 -0400

    initial launch screen

--- docs/notebooks/monte_carlo_analysis/gui/script.py
+++ docs/notebooks/monte_carlo_analysis/gui/script.py
@@ -1,69 +1,84 @@
-import pygame 
-import sys 
+import pygame
+import sys
+import matplotlib as mpl
 
-from time import process_time, time 
-from rocketpy import Environment, SolidMotor, Rocket, Flight
-import numpy as np 
-from numpy.random import normal, choice
-from IPython.display import display
-import matplotlib as mpl 
-import matplotlib.pyplot as plt
+pygame.init()
 
-pygame.init()  
-
-screen_width = 800
-screen_height = 600 
+screen_width = 1000
+screen_height = 1000
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('RocketPY GUI')
 
-running = True 
+running = True
 
-#colors
+# Colors
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 
-#fonts
+# Image
+image_path = './images/rocket.jpg'
+image = pygame.image.load(image_path)
+
+# Desired image size
+desired_width = 400
+aspect_ratio = image.get_height() / image.get_width()
+scaled_image = pygame.transform.scale(image, (desired_width, int(desired_width * aspect_ratio)))
+image_rect = scaled_image.get_rect(center=(screen_width // 2, screen_height // 2))
+
+button_text = 'Get Started'
+
+# Fonts
 title_font = pygame.font.Font(None, 50)
 subtitle_font = pygame.font.Font(None, 32)
 text_font = pygame.font.Font(None, 18)
+button_font = pygame.font.Font(None, 36)
 
-#rendering init
+# Rendering
 title_surface = title_font.render('Rocketpy GUI', True, BLACK)
-subtitle_surface = subtitle_font.render('monte carlo sims done easy', True, BLACK)
-text_surface = text_font.render('Some additional text.', True, BLACK)
+subtitle_surface = subtitle_font.render('Monte Carlo sims made easy', True, BLACK)
+text_surface = text_font.render('Made by Jaival Patel @ 2024', True, BLACK)
+button_text_surface = button_font.render(button_text, True, WHITE)
 
 
-#rect and pos
+
 title_rect = title_surface.get_rect(center=(screen_width // 2, 100))
 subtitle_rect = subtitle_surface.get_rect(center=(screen_width // 2, 150))
-text_rect = text_surface.get_rect(center=(screen_width // 2, 300))
+text_rect = text_surface.get_rect(center=(screen_width // 2, 900))
+button_rect = pygame.Rect(400, 400, 200, 50)
+button_text_rect = button_text_surface.get_rect(center=button_rect.center)
+
+button_color = (0, 128, 0)
+
+
+button_clicked = False
+
+
 
 while running:
-
-    #setting matplotlib stuff 
-    mpl.rcParams["figure.figsize"] = [8, 5]
-    mpl.rcParams["figure.dpi"] = 120 
-    mpl.rcParams["font.size"] = 14 
-    mpl.rcParams["legend.fontsize"] = 14 
-    mpl.rcParams["figure.titlesize"] = 14
-
-
-
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False 
-        
-    
+            running = False
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1: #checking left mouse button clicked
+                if button_rect.collidepoint(event.pos):
+                    button_clicked = not button_clicked
+
     screen.fill(WHITE)
 
-    #content 
-    screen.blit(title_surface, title_rect)
-    screen.blit(subtitle_surface, subtitle_rect)
-    screen.blit(text_surface, text_rect)
 
+    if button_clicked:
+        button_text_surface = button_font.render('Clicked!', True, WHITE)
+    else:      
+        screen.blit(scaled_image, image_rect)
+        screen.blit(title_surface, title_rect)
+        screen.blit(subtitle_surface, subtitle_rect)
+        screen.blit(text_surface, text_rect)
+        pygame.draw.rect(screen, button_color, button_rect)
+        screen.blit(button_text_surface, button_text_rect)
+        button_text_surface = button_font.render('Get Started!', True, WHITE)
 
-    #Updating 
+    button_text_rect = button_text_surface.get_rect(center=button_rect.center)
+
     pygame.display.flip()
 
 pygame.quit()
